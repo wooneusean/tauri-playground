d501520ddc5cecd6b7ff506ff47384cefd4f2742
snappymob-woon <146707643+snappymob-woon@users.noreply.github.com>
2024-09-17 02:33:54 +0800

    chore: initial project setup
    Frontend:
    
    - Add tailwind and dependencies (postcss, autoprefixer, etc)
    - Add mantine
    
    Backend:
    
    - Add `window-shadows` for nicer-looking decoration-less windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 mod commands {
     pub mod auth;
     pub mod profile;
@@ -12,6 +14,12 @@ fn main() {
             commands::auth::login,
             commands::profile::greet
         ])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window_shadows::set_shadow(window, true).unwrap();
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createTheme, MantineProvider } from '@mantine/core';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const theme = createTheme({});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
